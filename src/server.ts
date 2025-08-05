48c4ee7f55a3a000db1b1197b693dc42a621c17c
NikhilMeshram01 <nikhil.m@mindfiresolutions.com>
2025-08-05 12:00:45 -0700

    updated the folder structure

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 
 import express from 'express'
 import dotenv from 'dotenv'
+
 dotenv.config()
 
 const server = express()
+ 
+// database connection
+
+// middlewares
+
+// routes
 
 const port = process.env.PORT || 5001
 server.listen(port, ()=>{
     console.log('server running on ', port)
 })
+
